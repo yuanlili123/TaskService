a81c60545db109b3c9bfa4993b6b6305864ef93e
PC-20140906HEPR\Administrator <814805645@qq.com>
2016-11-16 14:25:55 +0800

    完成了NPC之间的交互

---
+++ src/Main.ts
@@ -0,0 +1,398 @@
+class Main extends egret.DisplayObjectContainer {
+
+    /**
+     * 加载进度界面
+     * Process interface loading
+     */
+    private loadingView:LoadingUI;
+
+    public static click : boolean;
+
+    public static getInstance(){
+
+        return this;
+    }
+
+    public constructor() {
+        super();
+        this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
+    }
+
+    private onAddToStage(event:egret.Event) {
+        //设置加载进度界面
+        //Config to load process interface
+        this.loadingView = new LoadingUI();
+        this.stage.addChild(this.loadingView);
+
+        //初始化Resource资源加载库
+        //initiate Resource loading library
+        RES.addEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
+        RES.loadConfig("resource/default.res.json", "resource/");
+    }
+
+    /**
+     * 配置文件加载完成,开始预加载preload资源组。
+     * configuration file loading is completed, start to pre-load the preload resource group
+     */
+    private onConfigComplete(event:RES.ResourceEvent):void {
+        RES.removeEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
+        RES.addEventListener(RES.ResourceEvent.ITEM_LOAD_ERROR, this.onItemLoadError, this);
+        RES.loadGroup("preload");
+    }
+
+    /**
+     * preload资源组加载完成
+     * Preload resource group is loaded
+     */
+    private onResourceLoadComplete(event:RES.ResourceEvent):void {
+        if (event.groupName == "preload") {
+            this.stage.removeChild(this.loadingView);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
+            RES.removeEventListener(RES.ResourceEvent.ITEM_LOAD_ERROR, this.onItemLoadError, this);
+            this.createGameScene();
+        }
+    }
+
+    /**
+     * 资源组加载出错
+     *  The resource group loading failed
+     */
+    private onItemLoadError(event:RES.ResourceEvent):void {
+        console.warn("Url:" + event.resItem.url + " has failed to load");
+    }
+
+    /**
+     * 资源组加载出错
+     *  The resource group loading failed
+     */
+    private onResourceLoadError(event:RES.ResourceEvent):void {
+        //TODO
+        console.warn("Group:" + event.groupName + " has failed to load");
+        //忽略加载失败的项目
+        //Ignore the loading failed projects
+        this.onResourceLoadComplete(event);
+    }
+
+    /**
+     * preload资源组加载进度
+     * Loading process of preload resource group
+     */
+    private onResourceProgress(event:RES.ResourceEvent):void {
+        if (event.groupName == "preload") {
+            this.loadingView.setProgress(event.itemsLoaded, event.itemsTotal);
+        }
+    }
+
+    private textfield:egret.TextField;
+
+    /**
+     * 创建游戏场景
+     * Create a game scene
+     */
+    
+
+
+    //对话任务面板的ui
+    public static dialogPanelContent1 = new egret.TextField();
+    public static dialogPanelContent2 = new egret.TextField();
+    public static dialogPanelContent3 = new egret.TextField();
+    public static dialogPanelContent4 = new egret.TextField();
+    public static dialogPanelButton = new egret.TextField();
+
+    public static taskPanelContent1 = new egret.TextField();
+    public static taskPanelContent2 = new egret.TextField();
+    public static taskPanelContent3 = new egret.TextField();
+    public static taskPanelContent4 = new egret.TextField();
+
+    //emoji
+    public static n0Emoji = new egret.TextField();
+    public static n1Emoji = new egret.TextField();
+
+    public showPanel(task : Task, tag : String){
+
+        if(tag == "taskpanel not accept" || tag == "taskpanel submit"){
+
+            Main.taskPanelContent1.x = 300;
+            Main.taskPanelContent1.y = 50;
+            Main.taskPanelContent1.size = 20;
+            Main.taskPanelContent1.text = "无进行中的任务";
+
+            Main.taskPanelContent2.text = "";
+            Main.taskPanelContent3.text = "";
+            Main.taskPanelContent4.text = "";
+
+        }else if(tag == "taskpanel accept"){
+
+            Main.taskPanelContent1.x = 300;
+            Main.taskPanelContent1.y = 50;
+            Main.taskPanelContent1.size = 20;
+            Main.taskPanelContent1.text = "任务名称: " + task.getName();
+
+            Main.taskPanelContent2.x = 300;
+            Main.taskPanelContent2.y = 100;
+            Main.taskPanelContent2.size = 20;
+            Main.taskPanelContent2.text = "发布任务NPC: " + task.getFromNpcId();
+
+            Main.taskPanelContent3.x = 300;
+            Main.taskPanelContent3.y = 150;
+            Main.taskPanelContent3.size = 20;
+            Main.taskPanelContent3.text = "完成任务NPC: " + task.getToNpcId();
+
+            Main.taskPanelContent4.x = 300;
+            Main.taskPanelContent4.y = 200;
+            Main.taskPanelContent4.size = 20;
+            Main.taskPanelContent4.text = "任务状态： " + task.getStatus();
+
+        }
+
+
+        if(tag == "accept"){
+
+            Main.dialogPanelContent1.x = 100;
+            Main.dialogPanelContent1.y = 600;
+            Main.dialogPanelContent1.text = "任务名称: " + task.getName();
+
+            Main.dialogPanelContent2.x = 200;
+            Main.dialogPanelContent2.y = 650;
+            Main.dialogPanelContent2.text = "发布任务NPC: " + task.getFromNpcId();
+
+            Main.dialogPanelContent3.x = 200;
+            Main.dialogPanelContent3.y = 700;
+            Main.dialogPanelContent3.text = "完成任务NPC: " + task.getToNpcId();
+
+            Main.dialogPanelContent4.x = 200;
+            Main.dialogPanelContent4.y = 750;
+            Main.dialogPanelContent4.text = "任务状态： " + task.getStatus();
+            
+            Main.dialogPanelButton.x = 300;
+            Main.dialogPanelButton.y = 800;
+            Main.dialogPanelButton.text = "接受";
+            Main.dialogPanelButton.touchEnabled = true;
+            Main.dialogPanelButton.addEventListener(egret.TouchEvent.TOUCH_TAP, function(){
+
+                
+                TaskService.getInstance().accept(task.getId());
+                Main.dialogPanelButton.text = "";
+                Main.dialogPanelContent1.text = "";
+                Main.dialogPanelContent2.text = "";
+                Main.dialogPanelContent3.text = "";
+                Main.dialogPanelContent4.text = "";
+
+            },this);
+   
+        }else if(tag == "finish"){
+
+            Main.dialogPanelContent1.x = 100;
+            Main.dialogPanelContent1.y = 600;
+            Main.dialogPanelContent1.text = "任务名称: " + task.getName();
+
+            Main.dialogPanelContent2.x = 200;
+            Main.dialogPanelContent2.y = 650;
+            Main.dialogPanelContent2.text = "发布任务NPC: " + task.getFromNpcId();
+
+            Main.dialogPanelContent3.x = 200;
+            Main.dialogPanelContent3.y = 700;
+            Main.dialogPanelContent3.text = "完成任务NPC: " + task.getToNpcId();
+
+            Main.dialogPanelContent4.x = 200;
+            Main.dialogPanelContent4.y = 750;
+            Main.dialogPanelContent4.text = "任务状态： " + task.getStatus();
+
+            Main.dialogPanelButton.x = 300;
+            Main.dialogPanelButton.y = 800;
+            Main.dialogPanelButton.text = "完成";
+            Main.dialogPanelButton.touchEnabled = true;
+            Main.dialogPanelButton.addEventListener(egret.TouchEvent.TOUCH_TAP, function(){
+
+                
+                TaskService.getInstance().finish(task.getId());
+                Main.dialogPanelButton.text = "";
+                Main.dialogPanelContent1.text = "";
+                Main.dialogPanelContent2.text = "";
+                Main.dialogPanelContent3.text = "";
+                Main.dialogPanelContent4.text = "";
+
+            },this);
+        }
+
+    }
+
+    public showEmoji(emoji : String){
+
+        if(emoji == "yellow !"){
+
+            Main.n0Emoji.text = "黄色叹号";
+            Main.n0Emoji.x = 40;
+            Main.n0Emoji.y = 220;
+            Main.n0Emoji.size = 20;
+            Main.n0Emoji.textColor = 0xFFFF00;
+
+        }else if(emoji == "yellow ?"){
+
+            Main.n0Emoji.text = "";
+
+
+            Main.n1Emoji.text = "黄色问号";
+            Main.n1Emoji.x = 340;
+            Main.n1Emoji.y = 530;
+            Main.n1Emoji.size = 20;
+            Main.n1Emoji.textColor = 0xFFFF00;
+
+        }else if(emoji == ""){
+
+            Main.n0Emoji.text = "";
+            Main.n1Emoji.text = "";
+        }
+
+
+    }
+
+    private createGameScene():void {
+
+        var bg:egret.Shape = new egret.Shape()
+        bg.graphics.beginFill( 0x336699 );
+        bg.graphics.drawRect( 0, 0, this.stage.stageWidth, this.stage.stageHeight ); 
+        bg.graphics.endFill();
+        this.addChild(bg);
+
+        this.touchEnabled = true;
+
+        var NPC0 = new NPC("npc_0");
+        var NPC1 = new NPC("npc_1");
+
+        var taskPanel = new TaskPanel("dialogpanel");
+        var taskAllTimePanel = new TaskPanel("taskpanel");
+        var taskService = TaskService.getInstance();
+
+        var task = new Task("0", "探索地图，找到隐藏的NPC", TaskStatus.ACCEPTABLE, "npc_0", "npc_1");
+        var task1 = new Task("1", "测试游戏系统的完成程度", TaskStatus.ACCEPTABLE, "npc_1", "npc_0");
+
+
+        var N0 = this.createBitmapByName("npc1_png");
+        N0.width = 156;
+        N0.height = 220;
+        N0.touchEnabled = true;
+        N0.addEventListener(egret.TouchEvent.TOUCH_TAP, function(){
+
+            Main.click = true;
+            TaskService.getInstance().notify(task);
+            
+        }, this);
+
+        this.addChild(N0);
+
+        var N1 = this.createBitmapByName("npc2_png");
+        N1.x = 300;
+        N1.y = 300;
+        N1.width = 131;
+        N1.height = 220;
+        N1.touchEnabled = true;
+        N1.addEventListener(egret.TouchEvent.TOUCH_TAP, function(){
+
+            Main.click = true;
+            TaskService.getInstance().notify(task);
+            
+        }, this);
+        this.addChild(N1);
+
+
+        this.addChild(Main.n0Emoji);
+        this.addChild(Main.n1Emoji);
+        this.addChild(Main.dialogPanelContent1);
+        this.addChild(Main.dialogPanelContent2);
+        this.addChild(Main.dialogPanelContent3);
+        this.addChild(Main.dialogPanelContent4);
+        this.addChild(Main.dialogPanelButton);
+
+        this.addChild(Main.taskPanelContent1);
+        this.addChild(Main.taskPanelContent2);
+        this.addChild(Main.taskPanelContent3);
+        this.addChild(Main.taskPanelContent4);
+
+
+        taskService.addObserver(NPC0);
+        taskService.addObserver(NPC1);
+        taskService.addObserver(taskPanel);
+        taskService.addObserver(taskAllTimePanel);
+        taskService.addTask(task);
+        taskService.addTask(task1);
+
+        taskService.getTaskByCustomRule(function(taskList : Task[]) : Task{
+
+            for(var i = 0; i < taskList.length; i++){
+
+                if(taskList[i].getStatus() == TaskStatus.ACCEPTABLE){
+
+                    taskService.notify(taskList[i]);
+                    return taskList[i];
+                }
+            }
+        });
+        
+    }
+
+    /**
+     * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
+     * Create a Bitmap object according to name keyword.As for the property of name please refer to the configuration file of resources/resource.json.
+     */
+    private createBitmapByName(name:string):egret.Bitmap {
+        var result = new egret.Bitmap();
+        var texture:egret.Texture = RES.getRes(name);
+        result.texture = texture;
+        return result;
+    }
+
+    /**
+     * 描述文件加载成功，开始播放动画
+     * Description file loading is successful, start to play the animation
+     */
+    private startAnimation(result:Array<any>):void {
+        var self:any = this;
+
+        var parser = new egret.HtmlTextParser();
+        var textflowArr:Array<Array<egret.ITextElement>> = [];
+        for (var i:number = 0; i < result.length; i++) {
+            textflowArr.push(parser.parser(result[i]));
+        }
+
+        var textfield = self.textfield;
+        var count = -1;
+        var change:Function = function () {
+            count++;
+            if (count >= textflowArr.length) {
+                count = 0;
+            }
+            var lineArr = textflowArr[count];
+
+            self.changeDescription(textfield, lineArr);
+
+            var tw = egret.Tween.get(textfield);
+            tw.to({"alpha": 1}, 200);
+            tw.wait(2000);
+            tw.to({"alpha": 0}, 200);
+            tw.call(change, self);
+        };
+
+        change();
+    }
+
+    /**
+     * 切换描述内容
+     * Switch to described content
+     */
+    private changeDescription(textfield:egret.TextField, textFlow:Array<egret.ITextElement>):void {
+        textfield.textFlow = textFlow;
+    }
+        private  touchNPC1(evt:egret.TouchEvent) {
+
+            console.log("click");
+
+        }
+}
+
+
---
+++ src/Task.ts
@@ -0,0 +1,288 @@
+class Task {
+    private id: String;
+    private name: string;
+    private desc: String;
+    private status: TaskStatus;
+    private fromNpcId: String;
+    private toNpcId: String;
+
+    constructor(id: String, name: string, status: TaskStatus, fromNpcId: String, toNpcId: String) {
+
+        this.id = id;
+        this.name = name;
+        this.status = status;
+        this.fromNpcId = fromNpcId;
+        this.toNpcId = toNpcId;
+
+    }
+
+    public getId(): String {
+
+        return this.id;
+
+    }
+
+    public getName(): string {
+
+        return this.name;
+    }
+
+    public getStatus(): TaskStatus {
+
+        return this.status;
+    }
+
+    public getFromNpcId(): String {
+
+        return this.fromNpcId;
+    }
+
+    public getToNpcId(): String {
+
+        return this.toNpcId;
+    }
+
+    public setStatus(taskStatus: TaskStatus): void {
+
+        this.status = taskStatus;
+    }
+
+}
+
+enum TaskStatus {
+
+    UNACCEPTABLE = 0,
+    ACCEPTABLE = 1,
+    DURING = 2,
+    CANSUBMIT = 3,
+    SUBMITTED = 4
+
+}
+
+enum ErrorCode {
+
+    SUCCESS,
+    MISSING_TASK
+
+}
+
+class TaskService {
+    //全局变量，一个模块最多一个（任务系统等）
+    private static instance: TaskService;
+    private static count = 0;
+    public taskList: Task[] = [];
+    public observerList: Observer[] = [];
+
+    constructor() {
+
+        TaskService.count++;
+        if (TaskService.count > 1) {
+            throw 'singleton!!!';
+        }
+    }
+
+    //获取TaskService的实例
+    public static getInstance() {
+
+        if (TaskService.instance == null) {
+
+            TaskService.instance = new TaskService();
+        }
+
+        return TaskService.instance;
+    }
+
+
+
+    //不公开数据，交给调用方处理
+    public getTaskByCustomRule(rule: Function): Task {
+
+        //拷贝数据
+        var clone = this.taskList;
+
+        //为传入函数增加了参数
+        return rule(clone);
+
+    }
+
+
+    public addTask(task: Task) {
+
+        this.taskList.push(task);
+    }
+
+
+    public addObserver(observer: Observer) {
+
+        this.observerList.push(observer);
+    }
+
+    //public removeObserver(observer : Observer){}
+    //public removeTask(task : Task){}
+
+
+    //完成任务时调用
+    public finish(id: String): ErrorCode {
+
+        if (id == "") {
+
+            return ErrorCode.MISSING_TASK;
+        }
+
+        for (var i = 0; i < this.taskList.length; i++) {
+
+            if (this.taskList[i].getId() == id) {
+
+                this.taskList[i].setStatus(TaskStatus.SUBMITTED);
+                this.notify(this.taskList[i]);
+                break;
+            }
+        }
+
+        return ErrorCode.SUCCESS;
+    }
+
+
+    //接受任务时调用
+    public accept(id: String): void {
+
+        for (var i = 0; i < this.taskList.length; i++) {
+
+            if (this.taskList[i].getId() == id && this.taskList[i].getStatus() == TaskStatus.ACCEPTABLE) {
+
+                this.taskList[i].setStatus(TaskStatus.DURING);
+                this.notify(this.taskList[i]);
+                break;
+            }
+        }
+
+    }
+
+
+    //将任务发送给所有观察者,并让观察者进行相应的处理
+    //只能内部调用
+    public notify(task: Task) {
+
+        for (var i = 0; i < this.observerList.length; i++) {
+
+            this.observerList[i].onChange(task);
+        }
+
+    }
+}
+
+
+
+class TaskPanel implements Observer {
+
+    private id: String;
+
+    constructor(id: String) {
+
+        this.id = id;
+    }
+
+    public getId(): String {
+
+        return this.id;
+    }
+
+
+    onChange(task: Task) {
+
+        if (task.getStatus() == TaskStatus.ACCEPTABLE) {
+
+            new Main().showPanel(task, "taskpanel not accept");
+        }
+
+        if (task.getStatus() == TaskStatus.DURING) {
+
+            new Main().showPanel(task, "taskpanel accept");
+        }
+
+        if (task.getStatus() == TaskStatus.SUBMITTED) {
+
+            new Main().showPanel(task, "taskpanel submit");
+        }
+
+        if (task.getStatus() == TaskStatus.ACCEPTABLE && Main.click) {
+
+            new Main().showPanel(task, "accept");
+
+        } else if (task.getStatus() == TaskStatus.DURING) {
+
+            new Main().showPanel(task, "finish")
+        }
+
+        Main.click = false;
+
+    }
+}
+
+
+class NPC implements Observer {
+
+    private id: String;
+
+    //头顶的提示任务状态的符号
+    private emoji: String;
+
+    constructor(id: String) {
+
+        this.id = id;
+    }
+
+    public getId(): String {
+
+        return this.id;
+    }
+
+    public setEmoji(emoji: String) {
+
+        this.emoji = emoji;
+    }
+
+
+    //根据变化的任务的相应状态改变相应NPC头顶的符号
+    onChange(task: Task) {
+
+        //任务刚创建时
+        if (task.getStatus() == TaskStatus.ACCEPTABLE) {
+
+            if (this.id == task.getFromNpcId()) {
+
+                this.emoji = "yellow !";
+
+            }
+
+        } else if (task.getStatus() == TaskStatus.DURING) {
+
+            if (this.id == task.getFromNpcId()) {
+
+
+                this.emoji = "";
+            }
+
+            if (this.id == task.getToNpcId()) {
+
+                this.emoji = "yellow ?";
+
+            }
+
+        } else if (task.getStatus() == TaskStatus.SUBMITTED) {
+
+            this.emoji = "";
+        }
+
+        new Main().showEmoji(this.emoji);
+
+    }
+
+}
+
+interface Observer {
+
+    //接受到信息后进行相应的处理，信息作为参数可以是任意事物，如task等
+    onChange(object: any);
+
+}
